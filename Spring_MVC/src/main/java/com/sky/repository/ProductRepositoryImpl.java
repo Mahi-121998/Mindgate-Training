0ed4eb8b48727f768c62440a69786d7539060cef
Mahi-121998 <105364300+Mahi-121998@users.noreply.github.com>
2022-05-21 05:05:14 -0700

    Add files via upload
    Spring_mvc

---
+++ Spring_MVC/src/main/java/com/sky/repository/ProductRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.sky.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sky.domain.Product;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+	
+	private static List<Product> products =new ArrayList<Product>();
+	@Override
+	public void addProduct(Product product) {
+		System.out.println("in reposiroty");
+		products.add(product);
+	}
+
+	@Override
+	public List<Product> getProduct() {
+		return products;
+	}
+
+}
